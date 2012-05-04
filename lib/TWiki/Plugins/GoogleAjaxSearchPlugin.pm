109ff8569b1dfa18883c2c225b31330632443079
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/GoogleAjaxSearchPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/GoogleAjaxSearchPlugin.pm
+++ lib/TWiki/Plugins/GoogleAjaxSearchPlugin.pm
@@ -2,7 +2,8 @@ package TWiki::Plugins::GoogleAjaxSearchPlugin;
 
 use strict;
 
-use vars qw( $VERSION $RELEASE $pluginName $debug $initialised $googleSiteKey $searchSite $searchWeb $siteLabel );
+use vars
+  qw( $VERSION $RELEASE $pluginName $debug $initialised $googleSiteKey $searchSite $searchWeb $siteLabel );
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -18,10 +19,10 @@ $pluginName = 'GoogleAjaxSearchPlugin';
 
 BEGIN {
     $initialised = 0;
-};
+}
 
 sub commonTagsHandler {
-	return unless ( $_[0] =~ s/%GOOGLEAJAXSEARCH%//ge );
+    return unless ( $_[0] =~ s/%GOOGLEAJAXSEARCH%//ge );
 
     if ( !$initialised ) {
         return unless _lazyInit();
@@ -29,7 +30,7 @@ sub commonTagsHandler {
 }
 
 sub _lazyInit {
-    unless ( $initialised ) {
+    unless ($initialised) {
         _addToHead();
         $initialised = 1;
     }
@@ -37,27 +38,42 @@ sub _lazyInit {
 
 #there is no need to document this.
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1.1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1.1 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
+    $debug = TWiki::Func::getPluginPreferencesFlag("DEBUG");
 
-    $debug = TWiki::Func::getPluginPreferencesFlag( "DEBUG" );
-    
-    $googleSiteKey = TWiki::Func::getPreferencesValue( 'GOOGLEKEY' ) || TWiki::Func::getPluginPreferencesValue( 'GOOGLEKEY' ) || '';
-   	$searchSite = TWiki::Func::getPreferencesValue( 'GOOGLESEARCHSITE' ) || TWiki::Func::getPluginPreferencesValue( "GOOGLESEARCHSITE" ) || '';
-	$searchWeb = TWiki::Func::getPreferencesValue( 'GOOGLESEARCHWEB' ) || TWiki::Func::getPluginPreferencesValue( "GOOGLESEARCHWEB" ) || '';
-	$siteLabel = TWiki::Func::getPreferencesValue( 'GOOGLESEARCHLABEL' ) || TWiki::Func::getPluginPreferencesValue( "GOOGLESEARCHLABEL" ) || '';
-		
+    $googleSiteKey =
+         TWiki::Func::getPreferencesValue('GOOGLEKEY')
+      || TWiki::Func::getPluginPreferencesValue('GOOGLEKEY')
+      || '';
+    $searchSite =
+         TWiki::Func::getPreferencesValue('GOOGLESEARCHSITE')
+      || TWiki::Func::getPluginPreferencesValue("GOOGLESEARCHSITE")
+      || '';
+    $searchWeb =
+         TWiki::Func::getPreferencesValue('GOOGLESEARCHWEB')
+      || TWiki::Func::getPluginPreferencesValue("GOOGLESEARCHWEB")
+      || '';
+    $siteLabel =
+         TWiki::Func::getPreferencesValue('GOOGLESEARCHLABEL')
+      || TWiki::Func::getPluginPreferencesValue("GOOGLESEARCHLABEL")
+      || '';
+
     return 1;
 }
 
 sub _addToHead {
-    my $header = '<script src="http://www.google.com/uds/api?file=uds.js&amp;v=1.0&amp;key='.$googleSiteKey.'" type="text/javascript"></script>';
+    my $header =
+'<script src="http://www.google.com/uds/api?file=uds.js&amp;v=1.0&amp;key='
+      . $googleSiteKey
+      . '" type="text/javascript"></script>';
     $header .= '
 <style type="text/css" media="all">
 	@import url("http://www.google.com/uds/css/gsearch.css");
@@ -67,7 +83,7 @@ sub _addToHead {
 <script type="text/javascript">
 	//<![CDATA[
 	GoogleAjaxSearch.prototype.getSearchSite = function () {
-		return "'.$searchSite.'" + this.getSearchWeb();
+		return "' . $searchSite . '" + this.getSearchWeb();
 	}
 	/**
 	An input field with id \'googleAjaxSearchWeb\' may override GOOGLESEARCHWEB.
@@ -79,7 +95,7 @@ sub _addToHead {
 			if (webName) return webName;
 		}
 		// else default
-		return "'.$searchWeb.'";
+		return "' . $searchWeb . '";
 	}
 	GoogleAjaxSearch.prototype.getUrlParam = function () {
 		return "%URLPARAM{"googleAjaxQuery" default=""}%";
@@ -91,12 +107,12 @@ sub _addToHead {
 			if (label) return label;
 		}
 		// else default
-		return "'.$siteLabel.'";
+		return "' . $siteLabel . '";
 	}
 //]]>
 </script>
 ';
-	TWiki::Func::addToHEAD('GOOGLEAJAXSEARCHPLUGIN',$header)
+    TWiki::Func::addToHEAD( 'GOOGLEAJAXSEARCHPLUGIN', $header );
 }
 
 1;
